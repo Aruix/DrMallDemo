86a6047ee22c3cf063041b8c43e8c238ab2e01f7
lidaoyuan.wb <lidaoyuan@sino-life.com>
2019-09-01 19:02:39 +0800

    跨域设置

--- litemall-core/src/main/java/org/linlinjava/litemall/core/config/CorsConfig.java
+++ litemall-core/src/main/java/org/linlinjava/litemall/core/config/CorsConfig.java
@@ -6,6 +6,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
     // 当前跨域请求最大有效时长。这里默认30天
@@ -14,8 +17,19 @@ public class CorsConfig {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("*"); // 1 设置访问源地址
-        corsConfiguration.addAllowedHeader("Origin, X-Requested-With, Content-Type, Accept"); // 2 设置访问源请求头
-        corsConfiguration.addAllowedMethod("POST, GET, PATCH, DELETE, PUT, OPTIONS"); // 3 设置访问源请求方法
+        List<String> origins = new ArrayList<>();
+        origins.add("http://litemall.lidaoyuan.top");
+        origins.add("http://localhost:9527");
+        corsConfiguration.setAllowedOrigins(origins);
+        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
+        List<String> methods = new ArrayList<>();
+        methods.add("POST");
+        methods.add("GET");
+        methods.add("PATCH");
+        methods.add("DELETE");
+        methods.add("PUT");
+        methods.add("OPTIONS");
+        corsConfiguration.setAllowedMethods(methods);
         corsConfiguration.setMaxAge(maxAge);
         corsConfiguration.setAllowCredentials(true);
 
